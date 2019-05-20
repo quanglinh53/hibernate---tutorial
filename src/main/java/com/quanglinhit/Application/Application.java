a2250a62340ab08cb8d8aacb85006a3db9869ced
John Baron <quanglinhd1097@gmail.com>
2019-05-20 07:25:13 +0700

    first commit

---
+++ src/main/java/com/quanglinhit/Application/Application.java
@@ -0,0 +1,49 @@
+package com.quanglinhit.Application;
+
+import java.util.List;
+
+import com.quanglinhit.Util.HibernateUtils;
+import com.quanglinhit.entity.Employee;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class Application {
+    public static void main(String[] args) {
+        SessionFactory factory = HibernateUtils.getSessionFactory();
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            // Tất cả các lệnh hành động với DB thông qua Hibernate
+            // đều phải nằm trong 1 giao dịch (Transaction)
+            // Bắt đầu giao dịch
+            session.getTransaction().begin();
+
+            // Tạo một câu lệnh HQL query object.
+            // Tương đương với Native SQL:
+            // Select e.* from EMPLOYEE e order by e.EMP_NAME, e.EMP_NO
+
+            String sql = "Select e from " + Employee.class.getName() + " e "
+                    + " order by e.empName, e.empNo ";
+
+            // Tạo đối tượng Query.
+            Query<Employee> query = session.createQuery(sql);
+
+            // Thực hiện truy vấn.
+            List<Employee> employees = query.getResultList();
+
+            for (Employee emp : employees) {
+                System.out.println("Emp: " + emp.getEmpNo() + " : "
+                        + emp.getEmpName());
+            }
+
+            // Commit dữ liệu
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Rollback trong trường hợp có lỗi xẩy ra.
+            session.getTransaction().rollback();
+        }
+    }
+}
